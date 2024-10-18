1a60e14eac79cf95bb5d1e6a66d97fd208eb56af
Nayuki <Kuuuuuuuu@users.noreply.github.com>
2024-10-18 19:47:35 +0700

    add NPC

---
+++ src/nayuki/commands/NPCCommand.php
@@ -0,0 +1,92 @@
+<?php
+
+declare(strict_types=1);
+
+namespace nayuki\commands;
+
+use nayuki\entities\NPC;
+use nayuki\Main;
+use nayuki\player\kit\BaseKit;
+use nayuki\player\kit\KitRegistry;
+use nayuki\Utils;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\nbt\tag\CompoundTag;
+use pocketmine\player\Player;
+use pocketmine\utils\TextFormat;
+use function count;
+use function is_string;
+
+final class NPCCommand extends Command{
+	public function __construct(private readonly Main $main){
+		parent::__construct('npc', 'NPC Command', '/npc help');
+		$this->setPermission('npc.command');
+	}
+
+	public function execute(CommandSender $sender, string $commandLabel, array $args) : void{
+		if(!$sender instanceof Player){
+			$sender->sendMessage(Main::PREFIX . TextFormat::RED . 'You can only use this command in-Game!');
+			return;
+		}
+
+		if(!$this->testPermission($sender)){
+			return;
+		}
+
+		if(isset($args[0])){
+			switch(strtolower($args[0])){
+				case 'spawn':
+					if(isset($args[1]) && is_string($args[1])){
+						$kit = KitRegistry::fromString($args[1]);
+						if($kit === false){
+							$sender->sendMessage(Main::PREFIX . TextFormat::RED . 'Kit not found!');
+							return;
+						}
+						$this->spawn($sender, $kit);
+					}else{
+						$sender->sendMessage(Main::PREFIX . TextFormat::RED . 'Usage: /npc spawn <kit>');
+					}
+					break;
+				case 'delete':
+					if(isset($args[1]) && is_numeric($args[1])){
+						$entity = $this->main->getServer()->getWorldManager()->findEntity((int) $args[1]);
+						if($entity instanceof NPC){
+							$entity->destroy();
+							$sender->sendMessage(Main::PREFIX . TextFormat::GREEN . 'NPC removed successfully!');
+						}else{
+							$sender->sendMessage(Main::PREFIX . TextFormat::YELLOW . 'NPC not found!');
+						}
+					}else{
+						$sender->sendMessage(Main::PREFIX . TextFormat::RED . 'Usage: /npc remove <id>');
+					}
+					break;
+				case 'list':
+					$entityNames = [];
+					foreach($sender->getWorld()->getEntities() as $entity){
+						if($entity instanceof NPC){
+							$entityNames[] = 'ID: ' . $entity->getId() . ' | Kit: ' . $entity->getKit()->getName();
+						}
+					}
+					$sender->sendMessage(Main::PREFIX . TextFormat::GREEN . 'NPCs: ' . count($entityNames) . "\n" . implode("\n", $entityNames));
+					break;
+				case 'help':
+					$sender->sendMessage("/npc spawn <kit> | /npc delete <id> | /npc list");
+					break;
+				default:
+					$sender->sendMessage(Main::PREFIX . TextFormat::RED . "Subcommand '$args[0]' not found! Try '/npc help' for help.");
+					break;
+			}
+		}
+	}
+
+	private function spawn(Player $player, BaseKit $kit) : void{
+		$nbt = Utils::createBaseNBT($player->getLocation(), null, $player->getLocation()->getYaw(), $player->getLocation()->getPitch());
+		$pos = $player->getLocation();
+		$nbt->setTag('Skin', CompoundTag::create()->setString('Name', $player->getSkin()->getSkinId())->setByteArray('Data', $player->getSkin()->getSkinData())->setByteArray('CapeData', $player->getSkin()->getCapeData())->setString('GeometryName', $player->getSkin()->getGeometryName())->setByteArray('GeometryData', $player->getSkin()->getGeometryData()));
+		$nbt->setString('kit', $kit->getName());
+		$entity = new NPC($pos, $player->getSkin(), $nbt);
+		$entity->spawnToAll();
+
+		$player->sendMessage(Main::PREFIX . TextFormat::GREEN . 'NPC spawned successfully! ID: ' . $entity->getId());
+	}
+}
---
+++ src/nayuki/entities/NPC.php
@@ -0,0 +1,86 @@
+<?php
+
+declare(strict_types=1);
+
+namespace nayuki\entities;
+
+use nayuki\player\kit\BaseKit;
+use nayuki\player\kit\KitRegistry;
+use pocketmine\entity\Human;
+use pocketmine\entity\Location;
+use pocketmine\entity\Skin;
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\event\entity\EntityDamageEvent;
+use pocketmine\nbt\tag\CompoundTag;
+use pocketmine\player\Player;
+use UnexpectedValueException;
+
+final class NPC extends Human{
+	protected float $gravity = 0.0;
+	protected BaseKit $kit;
+
+	public function __construct(Location $loc, Skin $skin, CompoundTag $nbt){
+		parent::__construct($loc, $skin, $nbt);
+		$kit = KitRegistry::fromString($nbt->getString('kit'));
+
+		if($kit === false){
+			$this->destroy();
+			return;
+		}
+
+		$this->kit = $kit;
+		$this->setCanSaveWithChunk(true);
+		$this->setNoClientPredictions();
+	}
+
+	public function saveNBT() : CompoundTag{
+		$nbt = parent::saveNBT();
+		$nbt->setString('kit', $this->kit->getName());
+		return $nbt;
+	}
+
+	public function attack(EntityDamageEvent $source) : void{
+		if(!($source instanceof EntityDamageByEntityEvent)){
+			return;
+		}
+
+		$damager = $source->getDamager();
+		if(!($damager instanceof Player)){
+			return;
+		}
+
+		$this->interact($damager);
+		$source->cancel();
+	}
+
+	private function interact(Player $player) : void{
+		$this->kit->setEffect($player);
+		$player->getInventory()->setContents($this->kit->getInventoryItems());
+		$player->getArmorInventory()->setContents($this->kit->getArmorItems());
+	}
+
+	public function destroy() : void{
+		if(!$this->isFlaggedForDespawn()){
+			$this->flagForDespawn();
+		}
+	}
+
+	public function getKit() : BaseKit{
+		return $this->kit;
+	}
+
+	protected function initEntity(CompoundTag $nbt) : void{
+		parent::initEntity($nbt);
+		$skinTag = $nbt->getCompoundTag('Skin');
+		if($skinTag === null){
+			throw new UnexpectedValueException('Missing skin data');
+		}
+		$this->setNameTagVisible();
+		$this->setNameTagAlwaysVisible();
+		$this->setScale(1.0);
+		$this->setNoClientPredictions();
+		$this->getInventory()->setContents($this->kit->getInventoryItems());
+		$this->getArmorInventory()->setContents($this->kit->getArmorItems());
+		$this->setNameTag($this->kit->getName());
+	}
+}
