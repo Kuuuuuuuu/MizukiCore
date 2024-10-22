5f4018110850fcabbb2b45ca07d0d418d658548f
Nayuki <Kuuuuuuuu@users.noreply.github.com>
2024-10-22 22:54:18 +0700

    init MainTask!

--- src/nayuki/tasks/MainTask.php
+++ src/nayuki/tasks/MainTask.php
@@ -4,12 +4,15 @@ declare(strict_types=1);
 
 namespace nayuki\tasks;
 
+use nayuki\Main;
 use nayuki\misc\AbstractTask;
 
 class MainTask extends AbstractTask{
-	protected function onUpdate(int $tick) : void{
-		if($tick % 20 === 0){
+	public function __construct(private readonly Main $main, int $period = 1){ parent::__construct($period); }
 
+	protected function onUpdate(int $tick) : void{
+		foreach($this->main->getSessionManager()->getSessions() as $session){
+			$session->update();
 		}
 	}
 }
