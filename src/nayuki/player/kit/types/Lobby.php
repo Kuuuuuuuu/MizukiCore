7e8c227c2f808dc45d46360aee7e29d8589ee9c8
Nayuki <Kuuuuuuuu@users.noreply.github.com>
2024-10-28 08:24:56 +0700

    ?

--- src/nayuki/player/kit/types/Lobby.php
+++ src/nayuki/player/kit/types/Lobby.php
@@ -10,6 +10,8 @@ use nayuki\player\kit\BaseKit;
 use nayuki\player\scoreboard\Scoreboard;
 use nayuki\player\scoreboard\ScoreboardUtils;
 use pocketmine\block\Block;
+use pocketmine\item\enchantment\EnchantmentInstance;
+use pocketmine\item\enchantment\VanillaEnchantments;
 use pocketmine\item\Item;
 use pocketmine\item\VanillaItems;
 use pocketmine\player\Player;
@@ -18,6 +20,7 @@ use pocketmine\utils\TextFormat;
 final class Lobby extends BaseKit{
 
 	private static string $settingsItemName = TextFormat::RESET . TextFormat::AQUA . 'Settings' . TextFormat::RESET . TextFormat::WHITE . ' (กดค้างเพื่อเปิดเมนู)';
+	private static string $shopItemName = TextFormat::RESET . TextFormat::AQUA . 'Shop' . TextFormat::RESET . TextFormat::WHITE . ' (กดค้างเพื่อเปิดเมนู)';
 
 	/**
 	 * @return Item[]
@@ -35,7 +38,8 @@ final class Lobby extends BaseKit{
 			VanillaItems::AIR(),
 			VanillaItems::AIR(),
 			VanillaItems::AIR(),
-			VanillaItems::COMPASS()->setCustomName(self::$settingsItemName)
+			VanillaItems::COMPASS()->setCustomName(self::$settingsItemName),
+			VanillaItems::BOOK()->setCustomName(self::$shopItemName)->addEnchantment(new EnchantmentInstance(VanillaEnchantments::FORTUNE(), 9)),
 		];
 	}
 
@@ -48,43 +52,43 @@ final class Lobby extends BaseKit{
 	}
 
 	public function handleItemSkill(Player $player, Item $itemOnHand) : void{
-		if($itemOnHand->getTypeId() !== VanillaItems::COMPASS()->getTypeId() || $itemOnHand->getCustomName() !== self::$settingsItemName){
-			return;
-		}
+		if($itemOnHand->getTypeId() === VanillaItems::BOOK()->getTypeId() && $itemOnHand->getCustomName() === self::$shopItemName){
+			// TODO: making some shop ui but rn I'm too sleepy to do it :p
+		}elseif($itemOnHand->getTypeId() === VanillaItems::COMPASS()->getTypeId() && $itemOnHand->getCustomName() === self::$settingsItemName){
+			$session = Main::getInstance()->getSessionManager()->getSession($player);
+			$form = new CustomForm(function(Player $player, array $data = null) use ($session) : void{
+				if($data === null){
+					return;
+				}
 
-		$session = Main::getInstance()->getSessionManager()->getSession($player);
-		$form = new CustomForm(function(Player $player, array $data = null) use ($session) : void{
-			if($data === null){
-				return;
-			}
+				$handlers = [
+					'cps' => function(bool $value) use ($session){
+						$session->setCpsCounterEnabled($value);
+					},
+					'scoreboard' => function(bool $value) use ($session, $player){
+						$session->setScoreboardEnabled($value);
 
-			$handlers = [
-				'cps' => function(bool $value) use ($session){
-					$session->setCpsCounterEnabled($value);
-				},
-				'scoreboard' => function(bool $value) use ($session, $player){
-					$session->setScoreboardEnabled($value);
+						if($value){
+							Scoreboard::spawn($player);
+						}else{
+							ScoreboardUtils::remove($player);
+						}
+					}
+				];
 
-					if($value){
-						Scoreboard::spawn($player);
-					}else{
-						ScoreboardUtils::remove($player);
+				foreach($data as $key => $value){
+					$handler = $handlers[strtolower($key)] ?? null;
+					if($handler !== null){
+						$handler((bool) $value);
 					}
 				}
-			];
+			});
 
-			foreach($data as $key => $value){
-				$handler = $handlers[strtolower($key)] ?? null;
-				if($handler !== null){
-					$handler((bool) $value);
-				}
-			}
-		});
+			$form->setTitle(TextFormat::AQUA . 'Settings');
+			$form->addToggle('CPS Counter', $session->isCpsCounterEnabled(), 'cps');
+			$form->addToggle('Scoreboard', $session->isScoreboardEnabled(), 'scoreboard');
 
-		$form->setTitle(TextFormat::AQUA . 'Settings');
-		$form->addToggle('CPS Counter', $session->isCpsCounterEnabled(), 'cps');
-		$form->addToggle('Scoreboard', $session->isScoreboardEnabled(), 'scoreboard');
-
-		$player->sendForm($form);
+			$player->sendForm($form);
+		}
 	}
 }
